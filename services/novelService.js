759bdce5703bd396959d71b678be2e40fede3ee9
vantukk2222 <ntu96109@gmail.com>
2024-06-02 17:25:37 +0700

    chore: Update sort order in novelService

--- services/novelService.js
+++ services/novelService.js
@@ -31,8 +31,8 @@ const novelService = {
     );
 
     nonEmptyNovels.sort((a, b) => {
-      if (a[sortField] < b[sortField]) return sortOrder === "desc" ? 1 : -1;
-      if (a[sortField] > b[sortField]) return sortOrder === "desc" ? -1 : 1;
+      if (a[sortField] < b[sortField]) return sortOrder === -1 ? 1 : -1;
+      if (a[sortField] > b[sortField]) return sortOrder === -1 ? -1 : 1;
       return 0;
     });
 
